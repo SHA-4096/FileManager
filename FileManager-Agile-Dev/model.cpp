4680c383f52ea0c56a22c19e0cb2ae6136ba57ca
n2ptr <xuzhanchaomail@163.com>
2024-02-20 12:05:35 +0800

    update[model]:sql文件初步生成

--- FileManager-Agile-Dev/model.cpp
+++ FileManager-Agile-Dev/model.cpp
@@ -1,10 +1,11 @@
 #include"model.h"
 
+#pragma region SqlScript
 
 
-SqlScript::SqlScript(){
+SqlScript::SqlScript() {
 	fp = nullptr;
-	fopen_s(&fp,"script.sql","wb");
+	fopen_s(&fp, "script.sql", "wb");
 }
 
 SqlScript::~SqlScript() {
@@ -20,7 +21,46 @@ SqlScript::~SqlScript() {
 /// <param name="host"></param>
 /// <param name="port"></param>
 /// <returns></returns>
-int SqlScript::InitScript(/*char* exePath, char* uName, char* pwd, char* host, char* port*/	) {
+int SqlScript::InitScript(/*char* exePath, char* uName, char* pwd, char* host, char* port*/) {
+	return 0;
+}
+
+/// <summary>
+/// 新增节点，时间格式化之后以字符串存储
+/// </summary>
+/// <param name="path"></param>
+/// <param name="attr"></param>
+/// <param name="time"></param>
+/// <param name="child"></param>
+/// <param name="sibling"></param>
+/// <param name="parent"></param>
+/// <returns></returns>
+int SqlScript::AddNode(TCHAR* path, DWORD attr, FILETIME time, int child, int sibling, int parent)
+{
+	auto SysTime = new SYSTEMTIME();
+	FileTimeToSystemTime(&time, SysTime);
+	TCHAR buf[MAX_PATHLEN*2];
+	auto formattedTime = new TCHAR[20];
+	_stprintf_s(formattedTime, 20, _T("%d-%d-%d"), SysTime->wYear, SysTime->wMonth, SysTime->wDay);
+	_stprintf_s(buf, MAX_PATHLEN*2, _T("INSERT INTO `file` (`path`, `attr`, `time`, `child`, `sibling`, `parent`) VALUES ('%ls', %d, %ls, %d, %d, %d);\n"), 
+		path, attr, formattedTime, child, sibling, parent);
+	this->AppendScript(buf);
+	return 0;
+}
+
+/// <summary>
+/// 更新p节点的Parent,Sibling以及Child
+/// </summary>
+/// <param name="NodeId"></param>
+/// <param name="Child"></param>
+/// <param name="Sibling"></param>
+/// <param name="Parent"></param>
+/// <returns></returns>
+int SqlScript::UpdateRelation(Node* p) {
+	TCHAR buf[MAX_PATHLEN * 2];
+	_stprintf_s(buf, MAX_PATHLEN * 2, _T("UPDATE `file` SET `child` = %d, `sibling` = %d, `parent` = %d WHERE `node_id` = %d;\n"),
+		CHILD_ID(p), SIBLING_ID(p), PARENT_ID(p), SELF_ID(p));
+	AppendScript(buf);
 	return 0;
 }
 
@@ -33,28 +73,35 @@ int SqlScript::AppendScript(TCHAR* buf) {
 	fwrite(buf, sizeof(TCHAR), _tcslen(buf), fp);
 	return 0;
 }
+#pragma endregion
 
-Node::Node(TCHAR* PathName,DWORD FileAttribute,FILETIME CreatedTime,int Depth) {
-	_tcscpy_s(this->PathName,MAX_PATHLEN ,PathName);
+#pragma region Node
+
+Node::Node(TCHAR* PathName, DWORD FileAttribute, FILETIME CreatedTime, int Depth, int NodeId) {
+	_tcscpy_s(this->PathName, MAX_PATHLEN, PathName);
+	this->NodeId = NodeId;
 	this->FileAttribute = FileAttribute;
 	this->CreatedTime = CreatedTime;
 	this->Depth = Depth;
-	this->Child = nullptr;
-	this->Sibling = nullptr;
+	this->Child = NULL;
+	this->Sibling = NULL;
 }
+#pragma endregion
 
+#pragma region DirectoryTree
 /// <summary>
 /// 构建目录树
 /// </summary>
 /// <param name="RootName"></param>
 DirectoryTree::DirectoryTree(TCHAR* RootPath) {
+	this->Script = new SqlScript();
 	setlocale(LC_ALL, "");
 	WIN32_FIND_DATA data;
 	TCHAR currentPath[MAX_PATHLEN];//用来做当前目录的搜索
 	TCHAR subDirPath[MAX_PATHLEN];//用来保存全路径
-	HANDLE hFind = FindFirstFile(RootPath,&data);
+	HANDLE hFind = FindFirstFile(RootPath, &data);
 	std::queue<Node*> dirQueue;
-	this->Root = new Node(RootPath, data.dwFileAttributes,data.ftCreationTime , 0);
+	this->Root = new Node(RootPath, data.dwFileAttributes, data.ftCreationTime, 0, this->IdAccumulator++);
 	dirQueue.push(Root);
 	int curDep = 0;
 	Node* curNode = Root;//根文件或要扫描的目录节点
@@ -73,7 +120,7 @@ DirectoryTree::DirectoryTree(TCHAR* RootPath) {
 				}
 				//新建节点
 				wsprintf(subDirPath, L"%ls\\%ls", curNode->PathName, data.cFileName);
-				appNode = new Node(subDirPath, data.dwFileAttributes, data.ftCreationTime, curDep + 1);
+				appNode = new Node(subDirPath, data.dwFileAttributes, data.ftCreationTime, curDep + 1, this->IdAccumulator++);
 				//深度统计
 				this->MaxDepth = (appNode->Depth > this->MaxDepth) ? appNode->Depth : this->MaxDepth;
 				//检查最长路径
@@ -82,14 +129,15 @@ DirectoryTree::DirectoryTree(TCHAR* RootPath) {
 
 				if (!(curNode->Child)) {
 					//添加该扫描结果为now的child					
-					auto err = this->AddChild(curNode,appNode);
+					auto err = this->AddChild(curNode, appNode);
 					if (err) {
 						printf_s("AddChild Error\n");
 						exit(-1);
 					}
-				}else {
+				}
+				else {
 					//添加该扫描结果为curNode->child的sibling
-					this->AddSibling(curNode->Child,appNode);
+					this->AddSibling(curNode->Child, appNode);
 				}
 				if (data.dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY) {
 					//如果是目录，加入队列
@@ -102,8 +150,9 @@ DirectoryTree::DirectoryTree(TCHAR* RootPath) {
 				}
 
 			} while (FindNextFile(hFind, &data));
-			
-		}else {
+
+		}
+		else {
 			wprintf(L"FindFirstFile failed at %ls\n", currentPath);
 		}
 	}
@@ -116,9 +165,9 @@ DirectoryTree::DirectoryTree(TCHAR* RootPath) {
 /// </summary>
 /// <param name="target"></param>
 /// <returns></returns>
-int DirectoryTree::AddSibling(Node* base,Node* target) {
+int DirectoryTree::AddSibling(Node* base, Node* target) {
 	Node* p = base;
-	while(p->Sibling){
+	while (p->Sibling) {
 		p = p->Sibling;
 	}
 	p->Sibling = target;
@@ -126,6 +175,9 @@ int DirectoryTree::AddSibling(Node* base,Node* target) {
 	target->Parent = base->Parent;
 	//更新最大深度
 	this->MaxRealDepth = max(target->RealDep, MaxRealDepth);
+	//Sql相关操作
+	this->Script->AddNode(target->PathName, target->FileAttribute, target->CreatedTime, 0, 0, target->Parent->NodeId);
+	this->Script->UpdateRelation(p);
 	return 0;
 }
 
@@ -134,7 +186,7 @@ int DirectoryTree::AddSibling(Node* base,Node* target) {
 /// </summary>
 /// <param name="target"></param>
 /// <returns></returns>
-int DirectoryTree::AddChild(Node* base,Node* target) {
+int DirectoryTree::AddChild(Node* base, Node* target) {
 	Node* p = base;
 	if (p->Child) {
 		//一个节点不能有多个Child
@@ -145,5 +197,10 @@ int DirectoryTree::AddChild(Node* base,Node* target) {
 	target->RealDep = base->RealDep + 1;
 	//更新最大深度
 	this->MaxRealDepth = max(target->RealDep, MaxRealDepth);
+	//Sql相关操作
+	this->Script->AddNode(target->PathName, target->FileAttribute, target->CreatedTime, 0, 0, target->Parent->NodeId);
+	this->Script->UpdateRelation(p);
 	return 0;
 }
+#pragma endregion
+
--- FileManager-Agile-Dev/model.h
+++ FileManager-Agile-Dev/model.h
@@ -1,32 +1,52 @@
 #pragma once
+#include<locale.h>
+#include<queue>
 #include<stdio.h>
 #include<tchar.h>
 #include<windows.h>
-#include<locale.h>
-#include<queue>
+#include<uuids.h>
 #define MAX_PATHLEN 1000
 
-class SqlScript {
-public:
-	SqlScript();
-	~SqlScript();
-	int InitScript(/*char* exePath, char* uName, char* pwd, char* host, char* port*/);//初始化脚本文件
-	int AppendScript(TCHAR* buf);//在文件中追加脚本
-private:
-	FILE* fp;
-};
+#define CHILD_ID(x) x->Child ? x->Child->NodeId:0
+#define PARENT_ID(x) x->Parent ? x->Parent->NodeId:0
+#define SIBLING_ID(x) x->Sibling ? x->Sibling->NodeId:0
+#define SELF_ID(x) x ? x->NodeId:0
+
+
+
 
 class Node {
 public:
-	Node(TCHAR*, DWORD, FILETIME, int);
+	Node(TCHAR*, DWORD, FILETIME, int Depth,int NodeId);
 	TCHAR PathName[MAX_PATHLEN];
 	DWORD FileAttribute;
 	FILETIME CreatedTime;
 	int Depth;//文件树深度
 	int RealDep;//数据结构真实深度
+	int NodeId;
 	Node* Child;
 	Node* Sibling;
 	Node* Parent;
+};
+
+/// <summary>
+/// 表项如下(默认主键等省略)
+/// +--------+--------+-------+-------+-------+--------+----------+
+/// |NodeId	 |FullPath|Child  |Sibling|Parent |FileAttr|CreateTime|
+/// +--------+--------+-------+-------+-------+--------+----------+
+/// </summary>
+class SqlScript {
+public:
+	SqlScript();
+	~SqlScript();
+	size_t CalcHash(TCHAR* path);//用path在内存中的地址作为参数计算hash值
+	int InitScript(/*char* exePath, char* uName, char* pwd, char* host, char* port*/);//初始化脚本文件
+	int AddNode(TCHAR* path, DWORD attr, FILETIME time, int child, int sibling, int parent);//在文件中追加一行)
+	int UpdateRelation(Node* p);//更新关系
+private:
+	FILE* fp;
+	std::hash<TCHAR*> hash;
+	int AppendScript(TCHAR* buf);//在文件中追加脚本
 };
 
 class DirectoryTree {
@@ -35,10 +55,14 @@ public:
 	int MaxRealDepth = 0;
 	int DirCount;
 	int FileCount;
+	int IdAccumulator = 0;//分配NodeId
 	TCHAR LongestFullPath[MAX_PATHLEN];
 	Node* Root;
 	DirectoryTree(TCHAR* RootName);
+
 private:
 	int AddSibling(Node* base,Node* target);
 	int AddChild(Node* base,Node* target);
+	SqlScript* Script;
 };
+
